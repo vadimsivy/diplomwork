c8d41ad7a0af787b4f330eb40a16ad8139204469
vadimsivy <merkvadim@gmail.com>
2022-07-07 23:28:02 +0300

    fixparcel

---
+++ dist/index.de5c0784.js
@@ -0,0 +1,6 @@
+$(".menu-burger, .menu-items").on("click", function() {
+    $(".menu-bg, .menu-items, .menu-burger").toggleClass("fs");
+    $(".menu-burger").text() == "\u2630" ? $(".menu-burger").text("\u2715") : $(".menu-burger").text("\u2630");
+});
+
+//# sourceMappingURL=index.de5c0784.js.map
