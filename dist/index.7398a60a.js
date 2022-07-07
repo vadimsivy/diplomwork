fdc93a25657789b1780c47cb7444fd6931524579
vadimsivy <merkvadim@gmail.com>
2022-07-08 00:00:11 +0300

    test2

---
+++ dist/index.7398a60a.js
@@ -0,0 +1,2 @@
+$(".menu-burger, .menu-items").on("click",(function(){$(".menu-bg, .menu-items, .menu-burger").toggleClass("fs"),"☰"==$(".menu-burger").text()?$(".menu-burger").text("✕"):$(".menu-burger").text("☰")}));
+//# sourceMappingURL=index.7398a60a.js.map
